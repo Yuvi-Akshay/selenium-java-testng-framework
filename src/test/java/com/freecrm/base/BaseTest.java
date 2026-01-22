b85118b7577c6bf42d18836583ef499f99a74f08
Akshay UV <akshayuv.0102@gmail.com>
2026-01-22 20:45:04 +0530

    adding loggers to the base class

--- src/test/java/com/freecrm/base/BaseTest.java
+++ src/test/java/com/freecrm/base/BaseTest.java
@@ -3,6 +3,9 @@ package com.freecrm.base;
 import java.time.Duration;
 import java.util.Properties;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -20,14 +23,17 @@ public class BaseTest {
     protected WebDriver driver;
     protected Properties prop;
     protected HomePage homePage;
+    protected static final Logger log = LogManager.getLogger(BaseTest.class);
 
     @BeforeMethod
     public void setUp() {
+        log.info("===== Test setup started =====");
         ConfigReader configReader = new ConfigReader();
         DriverFactory driverFactory = new DriverFactory();
 
         prop = configReader.initProp();
         String browser = prop.getProperty("browser");
+        log.info("Browser from config: {}", browser);
 
         driver = driverFactory.initDriver(browser);
 
@@ -36,26 +42,24 @@ public class BaseTest {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get(prop.getProperty("url"));
 
-        LandingPage landingPage = new LandingPage(driver);
-        LoginPage loginPage = landingPage.clickOnLoginBtn();
-        // homePage = loginPage.login(prop.getProperty("username"),
-        // prop.getProperty("password"));
-
-        // String userName = System.getenv("username");
-        // String password = System.getenv("password");
-        // System.out.println("Credentials loaded from environment");
-        // homePage = loginPage.login(userName, password);
+        log.info("Browser launched and configured successfully");
 
         Dotenv dotenv = Dotenv.configure().directory(System.getProperty("user.dir")).ignoreIfMissing().load();
         String userName = dotenv.get("FREECRM_USERNAME");
         String password = dotenv.get("FREECRM_PASSWORD");
+        log.info("Credentials loaded from environment");
+
+        LandingPage landingPage = new LandingPage(driver);
+        LoginPage loginPage = landingPage.clickOnLoginBtn();
         homePage = loginPage.login(userName, password);
 
     }
 
     @AfterMethod
     public void tearDown() {
+        log.info("===== Test execution finished =====");
         driver.quit();
+        log.info("Browser closed");
     }
 
 }
--- src/test/java/com/freecrm/base/BaseTestPublic.java
+++ src/test/java/com/freecrm/base/BaseTestPublic.java
@@ -6,6 +6,9 @@ import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.freecrm.utils.ConfigReader;
 import com.freecrm.utils.DriverFactory;
 
@@ -13,21 +16,28 @@ public class BaseTestPublic {
 
     protected WebDriver driver;
     protected Properties prop;
+    protected static final Logger log = LogManager.getLogger(BaseTestPublic.class);
 
     @BeforeMethod
     public void setUp() {
+        log.info("===== Test setup started =====");
         ConfigReader configReader = new ConfigReader();
         DriverFactory driverFactory = new DriverFactory();
 
         prop = configReader.initProp();
-        driver= driverFactory.initDriver(prop.getProperty("browser"));
+        driver = driverFactory.initDriver(prop.getProperty("browser"));
 
         driver.get(prop.getProperty("url"));
         driver.manage().window().maximize();
+
+        log.info("Browser launched and configured successfully");
+
     }
 
     @AfterMethod
     public void tearDown() {
+        log.info("===== Test execution finished =====");
         driver.quit();
+        log.info("Browser closed");
     }
 }
